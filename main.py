578b24a6a7b67c140e05cd2d14fbc2f3ef4ce50f
Vlad96Kir <130667144+Vlad96Kir@users.noreply.github.com>
2023-06-22 22:09:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,58 @@
 from datetime import datetime, timedelta
+from collections import defaultdict
 
-
-def get_birthdays_per_week(users):
-    # Отримати поточну дату
-    current_date = datetime.now().date()
-
-    # Знайти перший день тижня (понеділок)
-    start_date = current_date - timedelta(days=current_date.weekday())
-
-    # Знайти кінцевий день тижня (неділя)
-    end_date = start_date + timedelta(days=6)
-
-    # Зміщення для привітань, якщо перший день тижня - неділя
-    offset = timedelta(days=1) if start_date.weekday() == 6 else timedelta(days=0)
-
-    # Створити словник для зберігання користувачів по днях тижня
-    birthdays_per_week = {
-        "Monday": [],
-        "Tuesday": [],
-        "Wednesday": [],
-        "Thursday": [],
-        "Friday": [],
-        "Saturday": [],
-        "Sunday": []
-    }
-
-    # Проходження по користувачам і визначення днів народження на тижні
-    for user in users:
-        birthday = user["birthday"].date()
-
-        # Визначення наступного дня народження, якщо припадає на вихідний
-        if birthday.weekday() > 4:
-            next_birthday = birthday + timedelta(days=(7 - birthday.weekday()) + offset.days)
-        else:
-            next_birthday = birthday
-
-        # Перевірка, чи наступне день народження потрапляє в діапазон тижня
-        if start_date <= next_birthday <= end_date:
-            # Визначення дня тижня наступного дня народження
-            day_of_week = next_birthday.strftime("%A")
-
-            # Додавання користувача до відповідного списку за днем тижня
-            birthdays_per_week[day_of_week].append(user["name"])
-
-    # Виведення списку користувачів по днях тижня
-    for day, users in birthdays_per_week.items():
-        if users:
-            print(f"{day}: {', '.join(users)}")
-
-
-# Перевірка
 users = [
-    {"name": "Olexandr", "birthday": datetime(2023, 5, 27)},
-    {"name": "Taras", "birthday": datetime(2023, 5, 28)},
-    {"name": "Vasyl`", "birthday": datetime(2023, 5, 29)},
-    {"name": "Olena", "birthday": datetime(2023, 5, 30)},
-    {"name": "Volodymyr", "birthday": datetime(2023, 5, 31)},
-    {"name": "David", "birthday": datetime(2023, 6, 1)},
-    {"name": "Stephania", "birthday": datetime(2023, 6, 2)},
-    {"name": "Ignat", "birthday": datetime(2023, 6, 3)}
+    {'name': 'Килим', 'birthday': datetime(1990, 6, 22)},
+    {'name': 'Жак', 'birthday': '24.06.1999'},
+    {'name': 'Влад', 'birthday': datetime(1988, 6, 23)},
+    {'name': 'Ян', 'birthday': datetime(1992, 6, 24)},
+    {'name': 'Борис', 'birthday': datetime(1990, 6, 25)},
+    {'name': 'Марія', 'birthday': '26.06.1999'},
+    {'name': 'Кароліна', 'birthday': datetime(1988, 6, 27)},
+    {'name': 'Олена', 'birthday': datetime(1992, 6, 28)},
+    {'name': 'Ніна', 'birthday': datetime(1988, 6, 29)},
+    {'name': 'Володимир', 'birthday': datetime(1992, 6, 30)}
 ]
 
-get_birthdays_per_week(users)
+
+def get_period() -> tuple[datetime.date, datetime.date]:
+    current_date = datetime.now()
+    start_period = current_date + timedelta(days=5 - current_date.weekday())
+    return start_period.date(), (start_period + timedelta(6)).date()
+
+
+def check_epl(list_of_emp: list) -> dict:
+    result = defaultdict(list)
+    current_year = datetime.now().year
+    start, end = get_period()
+
+    for user in list_of_emp:
+        bd = user["birthday"].date() if isinstance(user["birthday"], datetime) else datetime.strptime(user["birthday"],
+                                                                                                      "%d.%m.%Y").date()
+        bd = bd.replace(year=current_year)
+
+        if bd.weekday() in (5, 6):
+                # Перевіряємо, якщо день народження випадає на вихідні, переносимо його на понеділок наступного тижня
+            while bd.weekday() != 0:  # 0 означає понеділок
+                bd += timedelta(days=1)
+
+        if start <= bd <= end:
+            result[bd].append(user["name"])
+
+    return result
+
+
+if __name__ == "__main__":
+    ukranian_days = {
+        0: 'Понеділок',
+        1: 'Вівторок',
+        2: 'Середа',
+        3: 'Четвер',
+        4: "П'ятниця",
+        5: 'Субота',
+        6: 'Неділя'
+    }
+
+    for key, value in check_epl(users).items():
+        day_of_week = ukranian_days[key.weekday()]
+        print(day_of_week, value)
